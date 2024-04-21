6c71226a2ef8cfabb9c59c3885410a4293807380
SladeBennett <slade8colt@gmail.com>
2024-04-21 17:55:40 -0600

    toggleCompleted efficient, only makes 1 server rqst

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -15,19 +15,19 @@ export default class App extends React.Component {
   onTodoNameInputChange = evt => {
     const { value } = evt.target
     this.setState({ ...this.state, todoNameInput: value })
-  } 
+  }
 
   resetForm = () => this.setState({ ...this.state, todoNameInput: '' })
 
-  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message })  
+  setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message })
 
   postNewTodo = () => {
     axios.post(url, { name: this.state.todoNameInput })
-    .then(res => {
-      this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
-      this.resetForm()
-    })
-    .catch(this.setAxiosResponseError)
+      .then(res => {
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+        this.resetForm()
+      })
+      .catch(this.setAxiosResponseError)
   }
 
   onTodoFormSubmit = evt => {
@@ -39,6 +39,17 @@ export default class App extends React.Component {
     axios.get(url)
       .then(res => {
         this.setState({ ...this.state, todos: res.data.data })
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleCompleted = id => () => {
+    axios.patch(`${url}/${id}`)
+      .then(res => {
+        this.setState({ ...this.state, todos: this.state.todos.map(td => {
+          if (td.id !== id) return td
+          return res.data.data
+        })})
       })
       .catch(this.setAxiosResponseError)
   }
@@ -55,7 +66,7 @@ export default class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? " ☻" : ''}</div>
             })
           }
         </div>
